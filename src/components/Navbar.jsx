49af37265d9ddb6b8e4882224a94c0b1ba6451ca
Amal Raj <amalraj2701@gmail.com>
2024-03-08 18:16:10 +0530

    Basic navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,70 @@
 function Navbar() {
-  return (
-    <></>
-  )
+	return (
+		<nav
+			className="navbar navbar-expand-lg bg-dark fixed-top"
+			data-bs-theme="dark"
+		>
+			<div className="container-fluid">
+				<div className="navbar-header">
+					<a href="#" target="_blank" className="navbar-brand" tabIndex={-1}>
+						Amal Raj
+					</a>
+				</div>
+				<button
+					className="navbar-toggler"
+					data-bs-toggle="offcanvas"
+					type="button"
+					data-bs-target="#offcanvasNavbar"
+					tabIndex={-1}
+					aria-controls="offcanvasNavbar"
+					aria-label="Toggle navigation"
+				>
+					<span className="navbar-toggler-icon"></span>
+				</button>
+				<div
+					className="offcanvas offcanvas-end"
+					aria-labelledby="offcanvasNavbarLabel"
+					id="offcanvasNavbar"
+				>
+					<div className="offcanvas-header">
+						<h5 className="offcanvas-title" id="offcanvasNavbarLabel">
+							Offcanvas
+						</h5>
+						<button
+							type="button"
+							className="btn-close"
+							data-bs-dismiss="offcanvas"
+							aria-label="Close"
+						></button>
+					</div>
+					<div className="offcanvas-body">
+						<ul className="nav navbar-nav">
+							<li className="nav-item">
+								<a href="" className="nav-link" aria-current="page">
+									Home
+								</a>
+							</li>
+							<li className="nav-item">
+								<a href="" className="nav-link">
+									Resume
+								</a>
+							</li>
+							<li className="nav-item">
+								<a href="" className="nav-link">
+									Projects
+								</a>
+							</li>
+							<li className="nav-item">
+								<a href="" className="nav-link">
+									Contact
+								</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</nav>
+	);
 }
 
-export default Navbar
+export default Navbar;
