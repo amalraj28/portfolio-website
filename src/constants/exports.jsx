a9a266ff564e237b861c05629a5cdf58d948c1f8
Amal Raj <amalraj2701@gmail.com>
2024-03-13 22:10:46 +0530

    Created file for exporting constants

--- src/constants/exports.jsx
+++ src/constants/exports.jsx
@@ -1,7 +1,28 @@
 export const jobs = [
-	"C++ Developer",
 	"Python Developer",
+	"Flutter Developer",
+	"C++ Developer",
 	"Front-End Web Developer",
-  "Flutter Developer",
 	"Deep Learning Enthusiast",
 ];
+
+export const aboutMe =
+	"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi doloribus officiis ducimus quos repudiandae unde rerum dignissimos nihil repellendus accusantium dolor neque, temporibus sint, commodi velit enim harum? Excepturi, quod?";
+
+export const projects = {
+	passwordManager: {
+		name: 'Password Manager App',
+		thumbnail: "src/assets/Amal Raj.jpg",
+		github: "https://github.com/amalraj28/password_manager",
+		description:
+			"A mobile application to manage passwords of users. Stores the user data locally on their devices. User can also generate random password if needed.",
+	},
+
+	calculatorReact: {
+		name: 'Calculator',
+		thumbnail: "src/assets/calculator_thumbnail.jpg",
+		github: "https://github.com/amalraj28/calculator-react",
+		description: "A basic calculator built using React and styled using SASS",
+		liveLink: "https://calculator-react-tlot.onrender.com/",
+	}
+};
