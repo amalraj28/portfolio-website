98823fd9c5f77a7d6bfc6807a22dd1a375a0f55b
Amal Raj <amalraj2701@gmail.com>
2024-03-14 07:13:13 +0530

    Fixed issue of content overlapping with Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,70 +1,73 @@
 function Navbar() {
 	return (
-		<nav
-			className="navbar navbar-expand-lg bg-dark fixed-top"
-			data-bs-theme="dark"
-			style={{top: "-0.20svh", width:"100vw"}}
-		>
-			<div className="container-fluid">
-				<div className="navbar-header">
-					<a href="#" target="_blank" className="navbar-brand" tabIndex={-1}>
-						Amal Raj
-					</a>
-				</div>
-				<button
-					className="navbar-toggler"
-					data-bs-toggle="offcanvas"
-					type="button"
-					data-bs-target="#offcanvasNavbar"
-					tabIndex={-1}
-					aria-controls="offcanvasNavbar"
-					aria-label="Toggle navigation"
-				>
-					<span className="navbar-toggler-icon"></span>
-				</button>
-				<div
-					className="offcanvas offcanvas-end"
-					aria-labelledby="offcanvasNavbarLabel"
-					id="offcanvasNavbar"
-				>
-					<div className="offcanvas-header">
-						<h5 className="offcanvas-title" id="offcanvasNavbarLabel">
-							Offcanvas
-						</h5>
-						<button
-							type="button"
-							className="btn-close"
-							data-bs-dismiss="offcanvas"
-							aria-label="Close"
-						></button>
+		<>
+			<nav
+				className="navbar navbar-expand-lg bg-dark fixed-top"
+				data-bs-theme="dark"
+				style={{top: "-0.20svh"}}
+			>
+				<div className="container-fluid">
+					<div className="navbar-header">
+						<a href="#" target="_blank" className="navbar-brand" tabIndex={-1}>
+							Amal Raj
+						</a>
 					</div>
-					<div className="offcanvas-body">
-						<ul className="nav navbar-nav">
-							<li className="nav-item">
-								<a href="" className="nav-link" aria-current="page">
-									Home
-								</a>
-							</li>
-							<li className="nav-item">
-								<a href="" className="nav-link">
-									Resume
-								</a>
-							</li>
-							<li className="nav-item">
-								<a href="" className="nav-link">
-									Projects
-								</a>
-							</li>
-							<li className="nav-item">
-								<a href="" className="nav-link">
-									Contact
-								</a>
-							</li>
-						</ul>
+					<button
+						className="navbar-toggler"
+						data-bs-toggle="offcanvas"
+						type="button"
+						data-bs-target="#offcanvasNavbar"
+						tabIndex={-1}
+						aria-controls="offcanvasNavbar"
+						aria-label="Toggle navigation"
+					>
+						<span className="navbar-toggler-icon"></span>
+					</button>
+					<div
+						className="offcanvas offcanvas-end"
+						aria-labelledby="offcanvasNavbarLabel"
+						id="offcanvasNavbar"
+					>
+						<div className="offcanvas-header">
+							<h5 className="offcanvas-title" id="offcanvasNavbarLabel">
+								Offcanvas
+							</h5>
+							<button
+								type="button"
+								className="btn-close"
+								data-bs-dismiss="offcanvas"
+								aria-label="Close"
+							></button>
+						</div>
+						<div className="offcanvas-body">
+							<ul className="nav navbar-nav">
+								<li className="nav-item">
+									<a href="" className="nav-link" aria-current="page">
+										Home
+									</a>
+								</li>
+								<li className="nav-item">
+									<a href="" className="nav-link">
+										Resume
+									</a>
+								</li>
+								<li className="nav-item">
+									<a href="" className="nav-link">
+										Projects
+									</a>
+								</li>
+								<li className="nav-item">
+									<a href="" className="nav-link">
+										Contact
+									</a>
+								</li>
+							</ul>
+						</div>
 					</div>
 				</div>
-			</div>
-		</nav>
+			</nav>
+			<div style={{paddingBottom: "70px"}}></div>
+		</>
 	);
 }
 
