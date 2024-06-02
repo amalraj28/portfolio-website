0778d1e5a5b42852edc3029e41a508b90b793daf
Amal Raj <amalraj2701@gmail.com>
2024-06-02 11:01:49 +0530

    Updated author section

--- src/components/Author.jsx
+++ src/components/Author.jsx
@@ -2,25 +2,19 @@ import AuthorName from "./AuthorName";
 import { aboutMe } from "../constants/exports";
 
 function Author({ selfRef }) {
-  return (
-    <section className="author" ref={selfRef}>
-      <div className="row author-details">
-      <AuthorName /> 
-       <div className="image col-lg-7">
-          <img
-            src="src\assets\Amal Raj.jpg"
-           
-            alt="Picture of author"
-          />
-        </div>
-      <div className="author-description ">
-          <p className="about-me">{aboutMe}</p>
-        </div>
-      </div>
-       
-      
-    </section>
-  );
+	return (
+		<section className="author" ref={selfRef}>
+			<div className="row author-details">
+				<AuthorName />
+				<div className="image col-lg-5">
+					<img src="src\assets\Amal Raj.jpg" alt="Picture of author" />
+				</div>
+				<div className="author-description">
+					<p className="about-me">{aboutMe}</p>
+				</div>
+			</div>
+		</section>
+	);
 }
 
 export default Author;
--- src/components/AuthorName.jsx
+++ src/components/AuthorName.jsx
@@ -4,12 +4,19 @@ import { jobs } from "../constants/exports";
 function AuthorName() {
 	return (
 		<div className="author-name col-lg-5">
-			<h3 className="name-text ">Hello, It&#39;s Me <br /></h3>
+			<h3 className="name-text">
+				Hello, It&#39;s Me <br />
+			</h3>
 			<h1>Amal Raj!</h1>
 			<div className="typewriter-text">
-				<h3>And I&#39;m a <span className="typewritter-span"> <TypeWriter jobs={jobs} /></span></h3>
+				<h3>
+					And I&#39;m a{" "}
+					<span className="typewritter-span">
+						{" "}
+						<TypeWriter jobs={jobs} />
+					</span>
+				</h3>
 			</div>
-			
 		</div>
 	);
 }
