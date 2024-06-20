ed33ede2a2596669447de5918ef0a6c5204d08fa
Amal Raj <amalraj2701@gmail.com>
2024-06-20 19:28:08 +0530

    Improved project section UI for laptop

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,34 +1,36 @@
-import "../styles/styles.scss";
 import { PropTypes } from "prop-types";
+import "../styles/styles.scss";
 
 function Project({ project }) {
-  const { name, thumbnail, liveLink, github, description } = project;
+	const { name, thumbnail, liveLink, github, description } = project;
 
-  return (
-    <div className="project-item">
-      <div className="card-deck">
-        <div className="card">
-          <img className="card-img-top" src={thumbnail} alt={description} />
-          <div className="card-body">
-            <h5 className="card-title">{name}</h5>
-            {liveLink !== undefined ? (
-              <a href={`${liveLink}`} target="_blank" rel="noopener noreferrer">
-                <button type="button">Live Link</button>
-              </a>
-            ) : (
-              <a href={github} target="_blank" rel="noopener noreferrer">
-                <button type="button">GitHub Link</button>
-              </a>
-            )}
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="project-item">
+			<div>
+				<img
+					src={thumbnail}
+					alt={description}
+					className="change-image-size"
+				/>
+				<div className="card-body">
+					<h5 className="card-title" id='project-title'>{name}</h5>
+					{liveLink !== undefined ? (
+						<a href={`${liveLink}`} target="_blank" rel="noopener noreferrer">
+							<button type="button" className="project-button">Live Link</button>
+						</a>
+					) : (
+						<a href={github} target="_blank" rel="noopener noreferrer">
+							<button type="button" className="project-button">GitHub Link</button>
+						</a>
+					)}
+				</div>
+			</div>
+		</div>
+	);
 }
 
 Project.propTypes = {
-  project: PropTypes.object,
+	project: PropTypes.object,
 };
 
 export default Project;
