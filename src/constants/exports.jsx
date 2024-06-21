a18c00585e9fed8b81aff27f02b26d1f8363fc8e
Amal Raj <amalraj2701@gmail.com>
2024-06-21 11:23:32 +0530

    Updated projects

--- src/constants/exports.jsx
+++ src/constants/exports.jsx
@@ -10,6 +10,14 @@ export const aboutMe =
 	"Enthusiastic and skilled college student with a robust foundation in technology and a passion for innovation. Proficient in a diverse array of programming languages including C++, Python, and Flutter, I thrive on tackling challenges and devising creative solutions. My continuous learning mindset keeps me at the forefront of emerging technologies, while my strong problem-solving abilities and organizational skills enable me to excel in dynamic environments. With experience in front-end web development and deep learning projects, I am eager to contribute my expertise and enthusiasm to a forward-thinking organization. Let's collaborate to drive technological advancements and achieve impactful results.";
 
 export const projects = {
+	portfolio: {
+		name: "Portfolio Website",
+		thumbnail: "/assets/portfolio.png",
+		github: "https://github.com/amalraj28/portfolio-website",
+		description: "My portfolio website built using React.js",
+		liveLink: "https:amalraj.vercel.app",
+	},
+
 	passwordManager: {
 		name: "Password Manager App",
 		thumbnail: "/assets/password_manager_thumbnail.jpg",
@@ -24,6 +32,21 @@ export const projects = {
 		github: "https://github.com/amalraj28/calculator-react",
 		description: "A basic calculator built using React and styled using SASS",
 		liveLink: "https://calculator-react-tlot.onrender.com/",
+	},
+
+	tictactoe: {
+		name: "Tic-Tac-Toe",
+		thumbnail: "/assets/tictactoe.png",
+		github: "https://github.com/amalraj28/tic-tac-toe",
+		description: "Tic-tac-toe game built using HTML, CSS and Javascript",
+		liveLink: "https://xoxojs.vercel.app",
+	},
+
+	pdfToAudioBook: {
+		name: "PDF to Audiobook Converter",
+		thumbnail: "/assets/pdf_to_audiobook.png",
+		github: "https://github.com/amalraj28/pdf_to_audiobook",
+		description: "Python script that reads out a PDF file"
 	},
 };
 
