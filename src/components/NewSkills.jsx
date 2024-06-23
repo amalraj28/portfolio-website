fe8512868cf976aa3d32ca6b07de98071d4b7f7f
Amal Raj <amalraj2701@gmail.com>
2024-06-23 19:24:08 +0530

    Basic structure of skills section is ready

---
+++ src/components/NewSkills.jsx
@@ -0,0 +1,51 @@
+import {
+	COriginalIcon,
+	CplusplusOriginalIcon,
+	CsharpOriginalIcon,
+	JavascriptOriginalIcon,
+	PythonOriginalIcon,
+	SequelizeOriginalIcon,
+} from "react-devicons";
+
+function NewSkills() {
+	return (
+		<>
+			<div className="new-skills">
+				<h3 className="skill-heading">Programming Languages</h3>
+				
+				<div className="skill-item">
+					<COriginalIcon className="skill-icon" size="3.5svw" />
+					<p className="skill-name">sds</p>
+				</div>
+				
+        <div className="skill-item">
+					<CplusplusOriginalIcon className="skill-icon" size="3.5svw" />
+					<p className="skill-name">sds</p>
+				</div>
+				
+        <div className="skill-item">
+					<PythonOriginalIcon className="skill-icon" size="3.5svw" />
+					<p className="skill-name">sds</p>
+				</div>
+				
+        <div className="skill-item">
+					<CsharpOriginalIcon className="skill-icon" size="3.5svw" />
+					<p className="skill-name">sds</p>
+				</div>
+				
+        <div className="skill-item">
+					<SequelizeOriginalIcon className="skill-icon" size="3.5svw" />
+					<p className="skill-name">sds</p>
+				</div>
+				
+        <div className="skill-item">
+					<JavascriptOriginalIcon className="skill-icon" size="3.5svw" />
+					<p className="skill-name">sds</p>
+				</div>
+				
+			</div>
+		</>
+	);
+}
+
+export default NewSkills;
